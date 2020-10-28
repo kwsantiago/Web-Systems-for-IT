f2b8b837256de5f2d3bc8a665b12cb11c660e8ab
kwsantiago <kwsantiago@usf.edu>
2020-10-27 21:16:22 -0400

    Completed functions

--- Modules/Module-8/8.13-UserRegistration/register.js
+++ Modules/Module-8/8.13-UserRegistration/register.js
@@ -4,31 +4,44 @@ function checkForm() {
     var password = document.forms["myForm"]["password"].value;
     var passwordConfirm = document.forms["myForm"]["passwordConfirm"].value;
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
-
-    if (fullName.length<1) {
-        document.getElementById('error-fullName').innerHTML = "Missing full name.";
-        document.getElementById('error-fullName').style.display = "inline";
+    if (fullName.length < 1) {
+        document.getElementById('errorList').innerHTML += '<li>Missing full name.</li>'
         document.getElementById('fullName').style.border = "2px solid red";
         document.getElementById('formErrors').style.display = "block";
     }
-    if (email.length<1 || regex.test(email)) {
-        document.getElementById('error-email').innerHTML = "Invalid or missing email address.";
-        document.getElementById('error-email').style.display = "inline";
+    if (email.length < 1 || !regex.test(email)) {
+        document.getElementById('errorList').innerHTML += '<li>Invalid or missing email address.</li>'
         document.getElementById('email').style.border = "2px solid red";
         document.getElementById('formErrors').style.display = "block";
     }
     if (password.length < 10 || password.length > 20) {
-        document.getElementById('error-password').innerHTML = "Password must be between 10 and 20 characters.";
-        document.getElementById('error-password').style.display = "inline";
+        document.getElementById('errorList').innerHTML += '<li>Password must be between 10 and 20 characters.</li>'
         document.getElementById('password').style.border = "2px solid red";
         document.getElementById('formErrors').style.display = "block";
     }
-    if (!(passwordConfirm === password)) {
-        document.getElementById('error-passwordConfirm').innerHTML = "Password and confirmation password don't match.";
-        document.getElementById('error-passwordConfirm').style.display = "inline";
+    if(!password.match(/[A-Z]/) || !password.match(/[a-z]/) || !password.match(/[0-9]/)){
+        if(!password.match(/[a-z]/)){
+            document.getElementById('errorList').innerHTML += '<li>Password must contain at least one lowercase character.</li>'
+            document.getElementById('password').style.border = "2px solid red";
+            document.getElementById('formErrors').style.display = "block";
+        }
+        if(!password.match(/[A-Z]/)){
+            document.getElementById('errorList').innerHTML += '<li>Password must contain at least one uppercase character.</li>'
+            document.getElementById('password').style.border = "2px solid red";
+            document.getElementById('formErrors').style.display = "block";
+        }
+        if(!password.match(/[0-9]/)){
+            document.getElementById('errorList').innerHTML += '<li>Password must contain at least one digit.</li>'
+            document.getElementById('password').style.border = "2px solid red";
+            document.getElementById('formErrors').style.display = "block";
+        }
+    }
+    if (passwordConfirm.length < 1 || !(passwordConfirm === password)) {
+        document.getElementById('errorList').innerHTML += "<li>Password and confirmation password don't match.</li>"
         document.getElementById('passwordConfirm').style.border = "2px solid red";
         document.getElementById('formErrors').style.display = "block";
     }
+    return false;
 }
 
 document.getElementById("submit").addEventListener("click", function(event) {
